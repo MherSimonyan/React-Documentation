c477a119361af549be17bd9ec3262a8478d176ab
mher20030606 <mher.simonyan.2014@bk.ru>
2019-11-12 22:27:44 +0400

    Updates

---
+++ precache-manifest.3451bde94fdc6ff56d8eaedeb5c10e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f228a1e386241297e85fccaff5ef0d7",
+    "url": "/Simer/index.html"
+  },
+  {
+    "revision": "6700cbbaaf13fa9e04bc",
+    "url": "/Simer/static/css/main.e654409a.chunk.css"
+  },
+  {
+    "revision": "bbc1c9eea8695ca21b28",
+    "url": "/Simer/static/js/2.5ec29c88.chunk.js"
+  },
+  {
+    "revision": "6700cbbaaf13fa9e04bc",
+    "url": "/Simer/static/js/main.2b3e3bd6.chunk.js"
+  },
+  {
+    "revision": "757abb01964a3955f307",
+    "url": "/Simer/static/js/runtime-main.381206bd.js"
+  },
+  {
+    "revision": "bdcaa185d0e5af31d911fa86529cc4af",
+    "url": "/Simer/static/media/bars-solid.bdcaa185.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Documentation/precache-manifest.2a4db47d329a74a699c57fb518c45681.js"
+  "/Simer/precache-manifest.3451bde94fdc6ff56d8eaedeb5c10e16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Documentation/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Simer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
